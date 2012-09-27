d0d5bd20ea476314233dfb04233e9290653a65cb
Trond Thorbjornsen <trondth@safir.ifi.uio.no>
2012-09-27 17:21:48 +0200

    Fjernet yes, my liege

--- src/GlobalConfig.py
+++ src/GlobalConfig.py
@@ -5,7 +5,7 @@ VERBOSE = False
 DEBUG = False
 IRC_DEBUG = False
 VERSION = 0.2
-NAME = "StupidBot"
+NAME = "computer"
 COMMAND_CHAR = "!"
 HELP_CHAR = '?'
 
@@ -16,9 +16,9 @@ GID = 1000
 HOST = "irc.ifi.uio.no"
 PORT = 6667
 NICK = NAME
-IDENT = NAME
+IDENT = "trondth"
 REAL_NAME = NAME
-OWNER = "Subfusc"
+OWNER = "Trond"
 STARTUP_CHANNELS = ['#iskbot']
 
 # ChannelManagerBot Config
@@ -28,10 +28,10 @@ STARTUP_CHANNELS = ['#iskbot']
 
 # LOGGER BOT config
 LOG_FILE = 'data/irc.log'
-LOG_BUFFER_SIZE = 1
+LOG_BUFFER_SIZE = 500
 
 # Plugin Bot config
-LOAD_PLUGINS = ["Kafe","Useless"]
+LOAD_PLUGINS = ["Useless" ]
 
 # CRON JOB BOT config
 START_CRON_BOT = True
@@ -39,12 +39,12 @@ START_CRON_BOT = True
 # Authentication System Config
 AUTHENTICATION = True
 RECOVER_USERS = True
-DATABASE_NAME = 'data/user_database.sql'
+DATABASE_NAME = 'data/user_database-chomsky.sql'
 HASH_ROUNDS = 200
-BOT_EMAIL = '' # must be set in order for the bot to send mail
-SMTP_SERVER = 'localhost' # Must be changed. smtp.uio.no i believe for hosting on IFI
+BOT_EMAIL = 'trondth@ifi.uio.no' # must be set in order for the bot to send mail
+SMTP_SERVER = 'smtp.uio.no' # Must be changed. smtp.uio.no i believe for hosting on IFI
 BOT_NICK = NAME
-DOMAIN_RESTRICTION = '' # For email, if you e.g want only *uio.no domains, you write .*uio\\.no instead of ''
+DOMAIN_RESTRICTION = '.*uio\\.no' # For email, if you e.g want only *uio.no domains, you write .*uio\\.no instead of ''
                         # '' is no restrictions. The domain restriction follow python regular expression rules.
 EMAIL_REGISTRATION = True # Allow users to use the same username that they have on their email
-FORCE_EMAIL_REGISTRATION = False # If you only want email nicks, this should be true
+FORCE_EMAIL_REGISTRATION = True # If you only want email nicks, this should be true
