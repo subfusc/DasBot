19092f26a5bb168394ab6baaf3d7b245b4ac5f2c
Sindre Wetjen <sindre.w@gmail.com>
2012-11-22 15:44:39 +0100

    Pulling new LoggerBot with new log format from alt-log

--- src/LoggerBot.py
+++ src/LoggerBot.py
@@ -11,27 +11,29 @@ class LoggerBot(AdminBot):
 
     def __init__(self):
         super(LoggerBot, self).__init__()
+        self.botname= "{n}@{h}".format(n = NAME, h = HOST)
+        self.dateformat = "[%F %R]"
         self.file = open(LOG_FILE, 'a', LOG_BUFFER_SIZE)
-        self.file.write("{n} Started at {d}\n".format(n = NAME, d = strftime("[%H:%M %Y/%m/%d]")))
+        self.file.write("{d} {n} started\n".format(n = self.botname, d = strftime(self.dateformat)))
         
     def __del__(self):
         super(LoggerBot, self).__del__()
-        self.file.write("{n} Stopped at {d}\n".format(n = NAME, d = strftime("[%H:%M %Y/%m/%d]")))
+        self.file.write("{d} {n} stopped\n".format(n = self.botname, d = strftime(self.dateformat)))
         self.file.close()
 
     def stop(self):
         super(LoggerBot,self).stop()
-        self.file.write("{n} Stopped at {d}\n".format(n = NAME, d = strftime("[%H:%M %Y/%m/%d]")))
+        self.file.write("{d} {n} stopped\n".format(n = self.botname, d = strftime(self.dateformat)))
         self.file.close()
         
     def _server_command(self, command, server):
         if not command == 'PING':
-            self.file.write("{d}: {s}:{c}\n".format(d = strftime("[%H:%M %Y/%m/%d]"),
+            self.file.write("{d} {s} {c}\n".format(d = strftime(self.dateformat),
                                                     s = server, c = command))
         super(LoggerBot, self)._server_command(command, server)
 
     def management_cmd(self, command, args, **kwargs):
-        self.file.write("{d}: {n}!{i}@{h}:{c} {a}\n".format(d = strftime("[%H:%M %Y/%m/%d]"),
+        self.file.write("{d} {n}!{i}@{h}:{c} {a}\n".format(d = strftime(self.dateformat),
                                                             n = kwargs['from_nick'],
                                                             i = kwargs['from_ident'],
                                                             h = kwargs['from_host_mask'],
@@ -39,7 +41,7 @@ class LoggerBot(AdminBot):
         super(LoggerBot, self).management_cmd(command, args, **kwargs)
         
     def cmd(self, command, args, channel, **kwargs):
-        self.file.write("{t}: {n}!{i}@{h} {c}:{cmd_char}{cmd} {a}\n".format(t = strftime("[%H:%M %Y/%m/%d]"),
+        self.file.write("{d} {n}!{i}@{h} {c} {cmd_char}{cmd} {a}\n".format(d = strftime(self.dateformat),
                                                                             n = kwargs['from_nick'],
                                                                             i = kwargs['from_ident'],
                                                                             h = kwargs['from_host_mask'],
@@ -49,7 +51,7 @@ class LoggerBot(AdminBot):
         super(LoggerBot, self).cmd(command, args, channel, **kwargs)
 
     def listen(self, command, msg, channel, **kwargs):
-        self.file.write("{d}: {n}!{i}@{h} {c}:{m}\n".format(d = strftime("[%H:%M %Y/%m/%d]"),
+        self.file.write("{d} {n}!{i}@{h} {c} {m}\n".format(d = strftime(self.dateformat),
                                                             c = channel,
                                                             n = kwargs['from_nick'],
                                                             i = kwargs['from_ident'],
@@ -58,7 +60,7 @@ class LoggerBot(AdminBot):
         super(LoggerBot, self).listen(command, msg, channel, **kwargs)
 
     def help(self, command, args, channel, **kwargs):
-        self.file.write("{d}: {n}!{i}@{h} {c}:{hc}{cmd} {a}\n".format(d = strftime("[%H:%M %Y/%m/%d]"),
+        self.file.write("{d} {n}!{i}@{h} {c} {hc}{cmd} {a}\n".format(d = strftime(self.dateformat),
                                                                       c = channel,
                                                                       n = kwargs['from_nick'],
                                                                       i = kwargs['from_ident'],
