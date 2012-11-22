e6de3fce83616ab89433467e4d07d736f0069f0e
Herman Torjussen <herman.torjussen@gmail.com>
2012-11-22 23:34:24 +0100

    logging normal, cmd and help messages; make log dir if not exist

--- src/plugins/Logger/__init__.py
+++ src/plugins/Logger/__init__.py
@@ -1,36 +1,80 @@
 # -*- coding: utf-8 -*- 
 from GlobalConfig import *
 from time import strftime
+from os import makedirs
 from os import path
 
 class Plugin(object):
 
     def __init__(self):
-        self.botname= "{n}@{h}".format(n = NAME, h = HOST)
+        self.botname = "{n}@{h}".format(n = NAME, h = HOST)
+        self.stdformat = "{d} {n}!{i}@{h} {m}\n"
+        self.hlpformat = "{d} {n}!{i}@{h} {hc}{cmd} {a}\n"
+        self.cmdformat = "{d} {n}!{i}@{h} {cc}{cmd} {a}\n"
         self.dateformat = "[%F %R]"
         self.prefix = "data/log/"
+        self.ext = ".log"
         
     def listen(self, msg, channel, **kwargs):
         if LOG_CHANNELS:
-            self.logchan(channel, msg, **kwargs)
-        
-    def logchan(self, chan, msg, **kwargs):
+            self.logchan(channel, self.stdformat, msg, **kwargs)
+
+    def cmd(self, command, args, channel, **kwargs):
+        if LOG_CHANNELS:
+            self.logchan(channel, self.cmdformat, [ command, args ], **kwargs)
+
+    def help(self, command, args, channel, **kwargs):
+        if LOG_CHANNELS:
+            self.logchan(channel, self.hlpformat, [ command , args ], **kwargs)
+    
+    
+    def logopen(self, chan):
         if not chan.startswith("#"):
-            print "not real channel"
-            return
-        logf = path.join(self.prefix, chan.strip("#"))
-        print "LOGFILE: "+logf
+            return None
+        if not path.exists(self.prefix):
+            makedirs(self.prefix)
+        
+        logfile = path.join(self.prefix, chan.strip("#") + self.ext)
+
         try:
-            f = open(logf, "a", LOG_BUFFER_SIZE)
+            chanlog = open(logfile, "a", LOG_BUFFER_SIZE)
         except IOError as e:
             print e
-            print "logfile "+logf+"does not exist -- open it"
-            return
-        f.write("{d} {n}!{i}@{h} {m}\n".format(d = strftime(self.dateformat),
-                                                c = chan,
-                                                n = kwargs['from_nick'],
-                                                i = kwargs['from_ident'],
-                                                h = kwargs['from_host_mask'],
-                                                m = msg))
-        f.close()
+            return None
+        return chanlog
+        
+        
+    def logchan(self, chan, lformat, msg, **kwargs):
+        chanlog = self.logopen(chan)
+        if chanlog:
+            if lformat == self.stdformat:
+                chanlog.write(lformat.format(
+                                    d = strftime(self.dateformat),
+                                    n = kwargs['from_nick'],
+                                    i = kwargs['from_ident'],
+                                    h = kwargs['from_host_mask'],
+                                    m = msg))
+            if lformat == self.cmdformat:
+                chanlog.write(lformat.format(
+                                    d = strftime(self.dateformat),
+                                    n = kwargs['from_nick'],
+                                    i = kwargs['from_ident'],
+                                    h = kwargs['from_host_mask'],
+                                    cmd = msg[0],
+                                    a = msg[1],
+                                    cc = COMMAND_CHAR))
+            if lformat == self.hlpformat:
+                chanlog.write(lformat.format(
+                                    d = strftime(self.dateformat),
+                                    n = kwargs['from_nick'],
+                                    i = kwargs['from_ident'],
+                                    h = kwargs['from_host_mask'],
+                                    cmd = msg[0],
+                                    a = msg[1],
+                                    hc = HELP_CHAR))
 
+            self.logclose(chanlog)
+
+    def logclose(self, log):
+        if log:
+            log.close()
