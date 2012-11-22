5594103fbed3a16263dfeb534f63dcbd0f87d27e
Sindre Wetjen <sindre.w@gmail.com>
2012-11-22 18:17:44 +0100

    reverting gloal config to default

--- src/GlobalConfig.py
+++ src/GlobalConfig.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # General Config Options
-VERBOSE = False
-DEBUG = False
+VERBOSE = True
+DEBUG = True
 IRC_DEBUG = False
 VERSION = 0.2
-NAME = "computer"
+NAME = "StupidBot"
 COMMAND_CHAR = "!"
 HELP_CHAR = '?'
 
@@ -16,24 +16,18 @@ GID = 1000
 HOST = "irc.ifi.uio.no"
 PORT = 6667
 NICK = NAME
-IDENT = "trondth"
+IDENT = "StupidBot"
 REAL_NAME = NAME
-OWNER = "Trond"
+OWNER = "Owner"
 STARTUP_CHANNELS = ['#iskbot']
 
-# ChannelManagerBot Config
-# Not in use ATM
-#PING_USERS = True
-#PING_DELAY = 10 * 60
-
 # LOGGER BOT config
-
 LOG_FILE = 'data/irc.log'
 LOG_CHANNELS = True
 LOG_BUFFER_SIZE = 1
 
 # Plugin Bot config
-LOAD_PLUGINS = ["Useless" ]
+LOAD_PLUGINS = ["Useless"]
 
 # CRON JOB BOT config
 START_CRON_BOT = True
@@ -41,12 +35,12 @@ START_CRON_BOT = True
 # Authentication System Config
 AUTHENTICATION = True
 RECOVER_USERS = True
-DATABASE_NAME = 'data/user_database-chomsky.sql'
+DATABASE_NAME = 'data/user_database.sql'
 HASH_ROUNDS = 200
-BOT_EMAIL = 'trondth@ifi.uio.no' # must be set in order for the bot to send mail
-SMTP_SERVER = 'smtp.uio.no' # Must be changed. smtp.uio.no i believe for hosting on IFI
+BOT_EMAIL = '' # must be set in order for the bot to send mail
+SMTP_SERVER = 'localhost' # Must be changed. smtp.uio.no i believe for hosting on IFI
 BOT_NICK = NAME
-DOMAIN_RESTRICTION = '.*uio\\.no' # For email, if you e.g want only *uio.no domains, you write .*uio\\.no instead of ''
+DOMAIN_RESTRICTION = '.*' # For email, if you e.g want only *uio.no domains, you write .*uio\\.no instead of ''
                         # '' is no restrictions. The domain restriction follow python regular expression rules.
 EMAIL_REGISTRATION = True # Allow users to use the same username that they have on their email
-FORCE_EMAIL_REGISTRATION = True # If you only want email nicks, this should be true
+FORCE_EMAIL_REGISTRATION = False # If you only want email nicks, this should be true
