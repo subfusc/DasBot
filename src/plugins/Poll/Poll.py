c1c8498095acb2c1c438083266debb72fd0a02af
Kristoffer Pedersen <deifyed@gmail.com>
2013-04-04 16:13:32 +0200

    New beginings

---
+++ src/plugins/Poll/Poll.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+import re
+import urllib
+import urllib2
+
+import GlobalConfig as conf
+
+# TODO
+# Make error messages
+
+POLL_LENGTH = 10                            # Poll default length
+POLL_HISTORY = True                         # Enable/disable poll history
+RESULT_FILE = r'data/pollresults'           # Poll history data location
+
+TIME_VALUE = { 'd':21600,
+        'h':3600,
+        'm':60,
+        's':1}
+
+class Poll(object):
+
+    def __init__(self, initiater, channel, question, alternatives, length):
+        self.question = question
+        self.alternatives = alternatives
+        self.winner = [ "", 0 ]
+        self.length = length
+
+        self.initiater = initiater
+
+        self.voters = [ ]
+
+        self.channel = channel
+        self.cronId = ()
+
+class PollBot(object):
+
+    def __init__(self):
+        self.activePoll = { }
+        self.keywords = ['or', 'eller', 'vs']
+        self.pollLength = POLL_LENGTH
+        self.POLL_HISTORY = POLL_HISTORY
+        self.timeRe = re.compile('(?P<number>\d+)(?P<timestamp>[s|m|h|d])((ecs?)|(ins?)|(ays?)|(ours?))?')
+        self.altRe = re.compile('[,\s?\w+,]')
+
+    def getAlternatives(self, argument):
+
+        words = argument.split(' ')
+
+        if words[1] in self.keywords and len(words) == 3:
+            if words[2][-1] == '?':
+                words[2] = words[2][:-1]
+
+            return { words[0]:0, words[2]:0 }
+
+        if '?' not in argument:
+            return None
+
+        words = argument.split('?')[1]
+        words = words.split(',')
+
+        alternatives = { }
+
+        for alt in words:
+            if alt != '':
+                alt = alt.strip()
+                if alt[0] != ',':
+                    alt = ',' + alt
+                if alt[-1] != ',':
+                    alt = alt + ','
+                if self.altRe.search(alt) == None:
+                    return None
+                alternatives[alt[1:-1]] = 0
+
+        if len(alternatives) < 2:
+            return None
+
+        return alternatives
+
+    def getQuestion(self, argument):
+                                            # TODO: Ikke bra nok. Noen kan skrive en hel historie her
+        if '?' not in argument:
+            return argument
+
+        return argument[:argument.index('?')+1]
+
+    def startPoll(self, initiater, channel, argument):
+
+        if channel in self.activePoll:
+            self.pollDebug('Channel is in self.activePoll')
+            return None
+
+        search = self.timeRe.search(argument)
+
+        if search != None:
+            argument = argument.split(' ', 1)[1]
+
+        alternatives = self.getAlternatives(argument)
+
+        if alternatives == None:
+            self.pollDebug('No alternatives found')
+            return None
+
+        question = self.getQuestion(argument)
+
+        if question == None:
+            self.pollDebug('No question found')
+            return None
+
+        if search != None:
+            time = int(search.group('number'))
+
+            time = time * TIME_VALUE[search.group('timestamp')]
+        else:
+            time = self.pollLength
+
+        if time > 2 * TIME_VALUE['d'] or time < 10:
+            return None
+
+        self.activePoll[channel] = Poll(initiater, channel, question, alternatives, time)
+
+        return 1
+
+    def endPoll(self, channel, kill):
+
+        if channel not in self.activePoll:
+            return None
+
+        if POLL_HISTORY:
+            f = open(RESULT_FILE, 'a')
+
+            f.write(self.activePoll[channel].question + ';' + self.activePoll[channel].winner[0] + ':' + str(self.activePoll[channel].winner[1]) + '\n')
+            f.flush()
+            f.close()
+
+        del(self.activePoll[channel])
+
+    def forceEndPoll(self, channel):
+        del(self.activePoll[channel])
+
+    def vote(self, channel, voter, alternative):
+
+        if channel not in self.activePoll:
+            return None
+
+        if voter in self.activePoll[channel].voters:
+            return None
+
+        alternative = alternative.lower().strip()
+
+        if alternative not in self.activePoll[channel].alternatives:
+            return None
+
+        self.activePoll[channel].voters.append(voter)
+        self.activePoll[channel].alternatives[alternative] += 1
+
+        return 1
+
+    def printPollHistory(self):
+        url = 'http://pastebin.com/api/api_post.php'
+
+        f = open(RESULT_FILE)
+
+        code = ''
+
+        for line in f:
+            line = line.split(';')
+            line[1] = line[1].split(':')
+
+            code += 'Question: "' + line[0] + '" with the winning answer: ' + line[1][0] + ' by ' + line[1][1][:-1] + ' votes.\n'
+
+        values = { 'api_option':'paste',
+                'api_paste_private':'1',
+                'api_paste_name':'Pollbot history',
+                'api_paste_expire_date':'10M',
+                'api_dev_key':'4c59086bba061d1c49277a824599343c',
+                'api_paste_code':code }
+
+        data = urllib.urlencode(values)
+        request = urllib2.Request(url, data)
+        response = urllib2.urlopen(request)
+        return(response.read())
+
+    def pollDebug(self, msg):
+        if conf.DEBUG:
+            print(msg)
---
+++ src/plugins/Poll/__init__.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+# TODO:
+# Draw, to med like mange stemmer
+# Case sensitivity
+
+import GlobalConfig as conf
+
+from Poll import PollBot
+from time import sleep
+import time
+
+VOTE_REPLY_ON_SUCCESS = 0
+VOTE_REPLY_ON_FAIL = 1
+
+class Plugin(object):
+
+    def __init__(self):
+        self.p = PollBot()
+
+    def stop(self):
+        del(self.p)
+
+    def help(self, command, argc, channel, **kwargs):
+        if command == 'poll':
+            return [(1, kwargs['from_nick'], conf.COMMAND_CHAR + "poll [length] [question]? alternative1, alternative2, ...")]
+
+    def halfway(self, channel):
+        return [(0, channel, 'The poll: "' + self.p.activePoll[channel].question + '" is half way done. Hurry up and vote.')]
+
+    def countdown(self, channel):
+        votes = []
+
+        for item in self.p.activePoll[channel].alternatives:
+            votes.append([self.p.activePoll[channel].alternatives[item], item])
+
+        votes = sorted(votes, reverse=True)
+
+        winnerCtr = 0
+        winners = ""
+
+        self.p.activePoll[channel].winner[1] = str(votes[0][0])
+
+        for item in votes:
+            if item[0] == votes[0][0]:
+                winnerCtr += 1
+
+        if winnerCtr > 1:
+            result = 'In the poll "' + self.p.activePoll[channel].question + '" the winner is tied between '
+
+            for i in range(0, winnerCtr):
+                result += votes[i][1] + ", "
+                winners += votes[i][1] + ", "
+
+            result = result.strip()[:-1]
+            result = result + ' with ' + str(votes[0][0]) + ' vote(s).'
+        else:
+            winners = votes[0][1]
+            result = 'In the poll "' + self.p.activePoll[channel].question + '" the winner is: ' + votes[0][1] + ' with ' + str(votes[0][0]) + ' vote(s).'
+
+        self.p.activePoll[channel].winner[0] = winners
+        self.p.endPoll(channel)
+
+        return [(0, channel, result)]
+
+    def cmd(self, command, args, channel, **kwargs):
+        if conf.DEBUG: print("COMMAND PollBot")
+        if command == 'poll':
+            if kwargs['auth_nick'] != None:
+                if args != "":
+                    response = self.p.startPoll(kwargs['from_nick'], channel, args)
+
+                    if response == None:
+                        return [(0, channel, "Vote failed to initiate.")]
+
+                    self.p.activePoll[channel].cronId = [ kwargs['new_job']((time.time() + int(self.p.activePoll[channel].length/2), self.halfway, [channel]))
+                                                         ,kwargs['new_job']((time.time() + self.p.activePoll[channel].length, self.countdown, [channel])) ]
+
+                    return [(0, channel, 'The poll: "' + args + '" has been initiated by ' + kwargs['from_nick'] +'.')]
+
+        if command == 'vote':
+            if channel in self.p.activePoll:
+                if args != None:
+                    if self.p.vote(channel, "{i}@{h}".format(i = kwargs['from_ident'], h = kwargs['from_host_mask']), args) == 1:
+                        if VOTE_REPLY_ON_SUCCESS == 1:
+                            return [(0, channel, 'Vote "' + args + '" registered from ' + kwargs['from_nick'])]
+                    else:
+                        if VOTE_REPLY_ON_FAIL == 1:
+                            print(self.p.activePoll[channel].alternatives) # TODO: Remove
+                            return [(0, channel, 'Vote "' + args + '" failed.')]
+                else:
+                    output = "The alternatives are: "
+
+                    for alt in self.p.activePoll[channel].alternatives:
+                        output += alt + ", "
+
+                    output = output[:-2] + "."
+                    return [(0, channel, output)]
+            else:
+                return [(0, channel, 'There is currently no active poll.')]
+
+        if command == 'endpoll':
+            if kwargs['auth_nick'] != None:
+                if channel in self.p.activePoll:
+                    kwargs['del_job'](self.p.activePoll[channel].cronId[0])
+                    kwargs['del_job'](self.p.activePoll[channel].cronId[1])
+
+                    self.p.forceEndPoll(channel)
+
+                    return [(0, channel, 'Poll probably ended successfully.')]
+
+                return [(0, channel, 'There is currently no active poll.')]
+
+        if command == 'pollhistory':
+            if self.p.POLL_HISTORY:
+                return [(0, channel, 'The poll history can be found at: ' + self.p.printPollHistory())]
